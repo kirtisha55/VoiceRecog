3a7700e190fe192574413087cf7b675261d60745
Kirti Sha <32695218+kirtisha55@users.noreply.github.com>
2019-04-09 16:45:38 +1000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace VoiceRecog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtvolume = new System.Windows.Forms.TextBox();
+            this.txtspeed = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Text";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(571, 59);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Volume";
+            // 
+            // txtvolume
+            // 
+            this.txtvolume.Location = new System.Drawing.Point(77, 119);
+            this.txtvolume.Name = "txtvolume";
+            this.txtvolume.Size = new System.Drawing.Size(63, 20);
+            this.txtvolume.TabIndex = 4;
+            // 
+            // txtspeed
+            // 
+            this.txtspeed.Location = new System.Drawing.Point(539, 119);
+            this.txtspeed.Name = "txtspeed";
+            this.txtspeed.Size = new System.Drawing.Size(61, 20);
+            this.txtspeed.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(473, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Speed";
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(177, 160);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(123, 23);
+            this.btnEnable.TabIndex = 7;
+            this.btnEnable.Text = "Enable Voice Control";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Enabled = false;
+            this.btnDisable.Location = new System.Drawing.Point(361, 160);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(130, 23);
+            this.btnDisable.TabIndex = 8;
+            this.btnDisable.Text = "Disable Voice Control";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtspeed);
+            this.Controls.Add(this.txtvolume);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtvolume;
+        private System.Windows.Forms.TextBox txtspeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+
+namespace VoiceRecog
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer _speak = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            _speak.Speak(str);
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtspeed.Text = "1";
+            txtvolume.Text = "100";
+            _speak.Rate = Convert.ToInt32(txtspeed.Text);
+            _speak.Volume = Convert.ToInt32(txtvolume.Text);
+
+            Choices commands = new Choices();
+            commands.Add(new string[] { "say Hello", "print my name"});
+            GrammarBuilder gbuilder = new GrammarBuilder();
+            gbuilder.Append(commands);
+            Grammar grammar = new Grammar(gbuilder);
+
+            _recognizer.LoadGrammarAsync(grammar);
+
+            //_recognizer = LoadDictationGrammars();
+
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.SpeechRecognized += _recognizer_SpeechRecognized;
+
+
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple); // recognize speech 
+            btnDisable.Enabled = true;
+        }
+
+        void _recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch(e.Result.Text)
+            {
+                case "say hello":
+                    _speak.Speak("Hello!");
+                    break;
+                case "print my name":
+                    textBox1.Text = "Name";
+                    break;
+                default:
+                    textBox1.Text = e.Result.Text;
+                    break;
+            }
+        }
+
+        private SpeechRecognitionEngine LoadDictationGrammars()
+        {
+
+            // Create a default dictation grammar.  
+            DictationGrammar defaultDictationGrammar = new DictationGrammar();
+            defaultDictationGrammar.Name = "default dictation";
+            defaultDictationGrammar.Enabled = true;
+
+            // Create the spelling dictation grammar.  
+            DictationGrammar spellingDictationGrammar =
+              new DictationGrammar("grammar:dictation#spelling");
+            spellingDictationGrammar.Name = "spelling dictation";
+            spellingDictationGrammar.Enabled = true;
+
+            // Create the question dictation grammar.  
+            DictationGrammar customDictationGrammar =
+              new DictationGrammar("grammar:dictation");
+            customDictationGrammar.Name = "question dictation";
+            customDictationGrammar.Enabled = true;
+
+            // Create a SpeechRecognitionEngine object and add the grammars to it.  
+            SpeechRecognitionEngine recoEngine = new SpeechRecognitionEngine();
+            recoEngine.LoadGrammar(defaultDictationGrammar);
+            recoEngine.LoadGrammar(spellingDictationGrammar);
+            recoEngine.LoadGrammar(customDictationGrammar);
+
+            // Add a context to customDictationGrammar.  
+            customDictationGrammar.SetDictationContext("How do you", null);
+
+            return recoEngine;
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            _recognizer.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+        }
+    }
+}
